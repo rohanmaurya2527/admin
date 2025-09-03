1d7eb0ee3a3d51ebbc30220d35db6e4ded8e4fe8
Rohan Maurya <rohanmaurya2527@gmail.com>
2025-09-03 11:52:40 +0530

    Update prac1.py

--- maths/prac1.py
+++ maths/prac1.py
@@ -1,1 +1,28 @@
-print("hello world")
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sn
+df=pd.read_csv("Datasets/risk_analytics_train - risk_analytics_train.csv",index_col=0)
+df.head()
+
+df.dtypes
+
+df.shape
+
+df.columns
+
+df.isnull().sum()
+
+df.describe(include='all')
+
+for x in ['Gender','Married','Dependents','Self_Employed','Loan_Amount_Term']:
+    df[x].fillna(df[x].mode()[0],inplace=True)
+df.isnull().sum()
+
+risk['LoanAmount'].fillna(round(risk['LoanAmount'].mean(),0),inplace=True)
+risk.isnull().sum()
+
+df.Credit_History.mode()
+
+risk['Credit_History'].fillna(value=0,inplace=True)
+risk.isnull().sum()
