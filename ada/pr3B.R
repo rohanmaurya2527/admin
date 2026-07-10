bc730b8e820407a7fbbd0a8eb081ec05e4e492e2
Rohan Maurya <rohanmaurya2527@gmail.com>
2026-07-10 11:55:29 +0530

    Create pr3B.R

---
+++ ada/pr3B.R
@@ -0,0 +1,18 @@
+#add 2 no
+add<-function(a,b){
+  res<-a+b
+  return(res)
+}
+print(add(5,6))
+#square
+square<-function(a){
+  res<-a**2
+  return(res)
+}
+print(square(5))
+#max
+maximum<-function(a,b){
+  res<-max(a,b)
+  return(res)
+}
+print(maximum(5,3))
