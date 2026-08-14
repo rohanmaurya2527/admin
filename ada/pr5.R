98e846c3f1ece40489d9b7c4920ea46570b0c765
Rohan Maurya <rohanmaurya2527@gmail.com>
2026-08-14 10:39:49 +0530

    Create pr5.R

---
+++ ada/pr5.R
@@ -0,0 +1,11 @@
+#Prac 5
+str<-"R Programming Language"
+print(nchar(str))
+print(toupper(str))
+print(tolower(str))
+print(substr(str,1,5))
+print(strrep("R",5))
+print(sub("Programming","Coding",str))
+print(strsplit(str," "))
+print(identical("R","R"))
+print(trimws("  R Programming   "))
